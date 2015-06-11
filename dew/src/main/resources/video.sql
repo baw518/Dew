84655cf73ec2e8849f429c68cd04e81c8d8b6df7
leejunghun7 <dlwjdgns6658@naver.com>
2015-06-11 15:37:21 +0900

    ergdfg

---
+++ dew/src/main/resources/video.sql
@@ -0,0 +1,74 @@
+SELECT video_no,title,hit,id,video_date FROM(
+	SELECT video_no,title,hit,id,video_date,CEIL(rownum/5) AS page FROM (
+		SELECT video_no,title,hit,id,to_char(video_date,'YYYY.MM.DD') 
+		as video_date FROM video_tutorial order by video_no desc
+	)
+) WHERE PAGE=1
+
+select video_no,title,hit,id,video_date,content
+		from video_tutorial
+		where video_no=#{value}
+		
+update video_tutorial set hit=hit+1 where video_no=1
+
+select count(*) from
+		video_tutorial
+		
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 목요일 공성','목요일공성',sysdate,'123123123')
+		
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 목요일 공성','금요일공성',sysdate,'123123123')
+		
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 목요일 공성','토요일공성',sysdate,'123123123')
+		
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 토요일 공성','일요일공성',sysdate,'123123123')
+		
+		
+insert into video_tutorial(video_no,id,title,content,video_date,video_path) 
+		values(video_no.nextval,'admin','세븐나이츠 목요일 공성','목요일공성',sysdate,'123123123')
