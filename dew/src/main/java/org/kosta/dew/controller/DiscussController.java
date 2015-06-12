b9f8d5fe58373ec8f3bad08c6b1516f276680235
KOSTA <KOSTA@KOSTA-HP>
2015-06-12 11:34:55 +0900

    윤지 sysout지우고 discuss.xml 수정함 150612 1134

--- dew/src/main/java/org/kosta/dew/controller/DiscussController.java
+++ dew/src/main/java/org/kosta/dew/controller/DiscussController.java
@@ -25,35 +25,27 @@ public class DiscussController {
 	 */
 	@RequestMapping("discussion_listView.do")
 	public ModelAndView getAllDiscussList(String pageNo){
-		System.out.println("토론방 컨트롤러");
-		System.out.println(pageNo);
 		return new ModelAndView("discussion_listView","dslvo",discussService.getAllDiscussList(pageNo));
 	}
 	@RequestMapping("findDiscussContent.do")
 	public ModelAndView findDiscussContent(HttpServletRequest request){
 		String no = request.getParameter("no");
-		System.out.println("파인드토론방 : " + no);
 		DiscussVO dsvo  = discussService.findDiscussContent(no);
-		System.out.println("파인드토론방 : "+dsvo);
 		return new ModelAndView("discussion_show_discussion","dsvo",dsvo);
 	}
 	@RequestMapping("show_discussion_comment.do")
 	@ResponseBody
 	public List<CommentVO> show_discussion_comment(HttpServletRequest request){
-		System.out.println("댓글");
 /*		CommentVO cvo = new CommentVO();*/
 		String discussionNo = request.getParameter("discussionNo");
-		System.out.println(discussionNo);
 		List<CommentVO> cmlist = discussService.findDiscussComment(discussionNo);
 		return cmlist;
 	}
 	@RequestMapping("deleteDiscussComment.do")
 	@ResponseBody
 	public ModelAndView deleteDiscussComment(HttpServletRequest request){
-		System.out.println("딜리트 댓글");
 		String no = request.getParameter("no");
 		String index = request.getParameter("index");
-		System.out.println("no:"+no+"   index:"+index);
 		discussService.deleteDiscussComment(no);
 		return new ModelAndView("redirect:findDiscussContent.do?no="+index);
 	}
